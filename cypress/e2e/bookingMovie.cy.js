ed34fa1e85220fe7ab7d8b622e4067230da923f2
Irina Khvatova <i.chwatowa@mail.ru>
2023-12-03 19:20:38 +0300

    Test added

--- cypress/e2e/bookingMovie.cy.js
+++ cypress/e2e/bookingMovie.cy.js
@@ -8,7 +8,19 @@ beforeEach(() => {
 it("Should booking a movie in an accessible hall, from admin panel", () => {
   cy.login(loginData.validEmail, loginData.validPass);
   cy.contains("Управление залами").should("be.visible");
-  cy.get(selector.sessionGrid).eq(5);
+  cy.get(selector.sessionGrid).eq(4);
   cy.get(selector.nameHall).click();
   cy.get(selector.openTicketSalesButton).click();
+});
+
+it("Should booking a ticket", () => {
+  cy.login(loginData.validEmail, loginData.validPass);
+  cy.contains("Управление залами").should("be.visible");
+  cy.visit("/");
+  cy.get('[data-time-stamp="1701810000"]').click();
+  cy.get(selector.chooseMovie).click();
+  cy.get(selector.movieSeance).contains("11:00").click();
+  cy.get(selector.seats).click();
+  cy.get(selector.reserve).click({ force: true });
+  cy.get(selector.bookingCode).click();
 });
